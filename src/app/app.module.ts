a2aee930c2c591229a9875cd346ef83af518eff0
Daniel Gomez Lobo Garcia <cuax@hotmail.es>
2020-04-19 19:38:08 +0200

    Usage of PrimeNG panels for professional profile sections.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -12,6 +14,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/professional-profile/professional-profile.module.ts
+++ src/app/professional-profile/professional-profile.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {PanelModule} from 'primeng/panel';
+
 import { ProfessionalProfileComponent } from './professional-profile.component';
 import { ProfessionalExperienceComponent } from './professional-experience/professional-experience.component';
 import { LanguagesComponent } from './languages/languages.component';
@@ -9,7 +11,8 @@ import { EducationComponent } from './education/education.component';
 @NgModule({
   declarations: [ProfessionalProfileComponent, ProfessionalExperienceComponent, LanguagesComponent, EducationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PanelModule
   ],
   exports: [
     ProfessionalProfileComponent
