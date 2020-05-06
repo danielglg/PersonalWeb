dd8265a3b5fb345645ea179dac67b397f93e95bf
Daniel Gomez Lobo Garcia <cuax@hotmail.es>
2020-05-06 23:28:03 +0200

    Chart information provided by service.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ChartInfoService } from './chart-info.service';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  providers: [ChartInfoService]
 })
 export class AboutComponent implements OnInit {
 
@@ -12,79 +15,12 @@ export class AboutComponent implements OnInit {
   dataDevelopment: any;
   data: any;
 
-  constructor() {
-    this.dataLanguages = {
-      labels: ['C++','Python','Java', 'Others'],
-      datasets: [
-          {
-              data: [57, 18, 15, 10],
-              backgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ],
-              hoverBackgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ]
-          }]
-      };
+  constructor(private chartInfoService: ChartInfoService) {
 
-    this.dataSectors = {
-      labels: ['Video','Networks','Payments', 'Others'],
-      datasets: [
-          {
-              data: [38.46, 15.38, 30.77, 15.38],
-              backgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ],
-              hoverBackgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ]
-          }]
-      };
-
-    this.dataDevelopment = {
-      labels: ['Backend','Frontend','Devops','Signal/data processing','Other'],
-      datasets: [
-          {
-              data: [60, 10, 10, 10, 10],
-              backgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ],
-              hoverBackgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ]
-          }]
-      };
-
-
-    this.data = {
-      labels: ['A','B','C'],
-      datasets: [
-          {
-              data: [300, 50, 100],
-              backgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ],
-              hoverBackgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ]
-          }]
-      };
+    this.dataLanguages   = this.chartInfoService.getLanguages();
+    this.dataSectors     = this.chartInfoService.getSectors();
+    this.dataDevelopment = this.chartInfoService.getDevelopment();
+    this.data            = this.chartInfoService.getRandomData();
   }
 
   ngOnInit(): void {
---
+++ src/app/about/chart-info.service.ts
@@ -0,0 +1,102 @@
+
+
+
+export class ChartInfoService {
+
+    dataLanguages: any;
+    dataSectors: any;
+    dataDevelopment: any;
+    data: any;
+
+    constructor()
+    {
+        this.dataLanguages = {
+          labels: ['C++', 'Python', 'Java', 'Others'],
+          datasets: [
+              {
+                  data: [57, 18, 15, 10],
+                  backgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ],
+                  hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ]
+              }]
+          };
+
+        this.dataSectors = {
+          labels: ['Video','Networks','Payments', 'Others'],
+          datasets: [
+              {
+                  data: [38.46, 15.38, 30.77, 15.38],
+                  backgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ],
+                  hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ]
+              }]
+          };
+
+        this.dataDevelopment = {
+          labels: ['Backend', 'Frontend', 'Devops', 'Signal/data processing', 'Other'],
+          datasets: [
+              {
+                  data: [60, 10, 10, 10, 10],
+                  backgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ],
+                  hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ]
+              }]
+          };
+
+        this.data = {
+          labels: ['A', 'B', 'C'],
+          datasets: [
+              {
+                  data: [300, 50, 100],
+                  backgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ],
+                  hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                  ]
+              }]
+          };
+    }
+
+
+    getLanguages() {
+        return this.dataLanguages;
+    }
+
+    getSectors() {
+        return this.dataSectors;
+    }
+
+    getDevelopment() {
+        return this.dataDevelopment;
+    }
+
+    getRandomData() {
+        return this.data;
+    }
+}
