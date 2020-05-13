7ecee5639c5bf6ddc376a68accffb36875650609
Daniel Gomez Lobo Garcia <cuax@hotmail.es>
2020-05-13 22:35:02 +0200

    Customer cards with buttons to projects and logos to websites.

--- src/app/professional-profile/professional-experience/professional-experience.component.ts
+++ src/app/professional-profile/professional-experience/professional-experience.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {MenuItem} from 'primeng/primeng';
 
 @Component({
   selector: 'app-professional-experience',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessionalExperienceComponent implements OnInit {
 
-  constructor() { }
+  amadeusItems: MenuItem[];
+  ericssonItems: MenuItem[];
+  nokiaItems: MenuItem[];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.amadeusItems = [
+      {label: 'EMV', icon: 'pi pi-refresh', routerLink: ['/projects/payments/emv']},
+      {label: 'AAO', icon: 'pi pi-refresh', routerLink: ['/projects/payments/acquiring']}
+    ];
+    this.ericssonItems = [
+      {label: 'SAPC', icon: 'pi pi-refresh', routerLink: ['/projects/networks/pcrf']},
+    ];
+    this.nokiaItems = [
+      {label: 'VPP', icon: 'pi pi-refresh', routerLink: ['/projects/video/personalization']},
+    ];
   }
+
+  goToProjects(projectName: string) {
+    if ( projectName == 'payments') {
+      this.router.navigateByUrl('/projects/payments');
+    }
+    else if ( projectName == 'networks') {
+      this.router.navigateByUrl('/projects/networks');
+    }
+    else if ( projectName == 'video') {
+      this.router.navigateByUrl('/projects/video');
+    }
+}
 
 }
