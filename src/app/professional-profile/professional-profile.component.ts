a20c04a41e9a14e012dedbb353ab5dc16f89ff90
Daniel Gomez Lobo Garcia <cuax@hotmail.es>
2020-05-14 20:51:30 +0200

    Basic CV page navigation sidebar.

--- src/app/professional-profile/professional-profile.component.ts
+++ src/app/professional-profile/professional-profile.component.ts
@@ -4,7 +4,7 @@ import { ProfessionalProfileModule } from './professional-profile.module';
 @Component({
   selector: 'app-professional-profile',
   templateUrl: './professional-profile.component.html',
-  styleUrls: ['./professional-profile.component.scss']
+  styleUrls: ['./professional-profile.component.scss', '../../assets/common_styles/projects.scss']
 })
 export class ProfessionalProfileComponent implements OnInit {
 
