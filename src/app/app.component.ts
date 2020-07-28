a16f2985eaa8a1cbb3eac3790e9e83641879edf8
Daniel Gomez Lobo Garcia <cuax@hotmail.es>
2020-07-29 00:22:07 +0200

    Web-app title and top-level meta tags.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  owner = 'Daniel Gómez-Lobo García';
+
+  public constructor(private titleService: Title, private meta: Meta ) {
+    this.setTitle(this.owner);
+    this.setMeta();
+  }
+
+  private setTitle( newTitle: string) {
+    this.titleService.setTitle( newTitle );
+  }
+
+  private setMeta() {
+    this.meta.addTags([
+      { name: 'author', content: this.owner },
+      { name: 'description', content: 'Professional website of ' + this.owner },
+      { name: 'keywords', content: 'CV, curriculum, projects, portfolio' }
+    ]);
+  }
 }
